9a4a85a528bf95fb6b2e3120e15d7cd7b7ffc4a7
dhileepbalaji <33687091+dhileepbalaji@users.noreply.github.com>
2020-04-26 11:07:04 +0530

    Create variables.tf

---
+++ modules/eventgrid/variables.tf
@@ -0,0 +1,15 @@
+variable "tf_lbl_cost_center" {}
+variable "tf_lbl_product" {}
+variable "tf_lbl_deployment_name" {}
+variable "tf_lbl_owner" {}
+variable "tf_lbl_environment" {}
+variable "tf_lbl_provisioned_by" {}
+variable "tf_lbl_source_repo" {}
+variable "tf_lbl_releaseid" {}
+variable "tf_lbl_buildid" {}
+variable "resource_group" {}
+variable "location" {}
+variable "eventgrid" {}
+variable "eventgridsubscription" {}
+variable "storageaccount" {}
+variable "subscriptionkey" {}
