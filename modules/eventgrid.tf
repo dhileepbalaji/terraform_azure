9910427fac24d42c361a2b853ae603479800b116
dhileepbalaji <33687091+dhileepbalaji@users.noreply.github.com>
2020-04-26 11:06:20 +0530

    Update eventgrid.tf

--- modules/eventgrid.tf
+++ modules/eventgrid.tf
@@ -1,42 +1,64 @@
+variable "tf_lbl_cost_center" {}
+variable "tf_lbl_product" {}
+variable "tf_lbl_deployment_name" {}
+variable "tf_lbl_owner" {}
+variable "tf_lbl_environment" {}
+variable "tf_lbl_provisioned_by" {}
+variable "tf_lbl_source_repo" {}
+variable "tf_lbl_releaseid" {}
+variable "tf_lbl_buildid" {}
+variable "resource_group" {}
+variable "location" {}
+variable "eventgrid" {}
+variable "eventgridsubscription" {}
+variable "storageaccount" {}
+variable "subscriptionkey" {}
+
+
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group}"
   location = "${var.location}"
 }
 
+data "azurerm_resource_group" "storageaccount" {
+  name = "${var.storageaccount}"
+}
+
 resource "azurerm_eventgrid_topic" "eventgridtopic" {
-  count               = "${length(var.flasheventgrid)}"
-  name                = "${lookup(var.flasheventgrid[count.index], "name")}"
+  count               = "${length(var.eventgrid)}"
+  name                = "${lookup(var.eventgrid[count.index], "name")}"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   tags = {
-    CostCenter          = "${var.mrc_lbl_cost_center}"
-    Product             = "${var.mrc_lbl_product}"
-    DeploymentName      = "${var.mrc_lbl_deployment_name}"
-    Owner               = "${var.mrc_lbl_owner}"
-    Environment         = "${var.mrc_lbl_environment}"
-    ProvisionedBy       = "${var.mrc_lbl_provisioned_by}"
-    SourceRepo          = "${var.mrc_lbl_source_repo}"
-    ReleaseId           = "${var.mrc_lbl_releaseid}"
-    BuildId             = "${var.mrc_lbl_buildid}"    
+    CostCenter          = "${var.tf_lbl_cost_center}"
+    Product             = "${var.tf_lbl_product}"
+    DeploymentName      = "${var.tf_lbl_deployment_name}"
+    Owner               = "${var.tf_lbl_owner}"
+    Environment         = "${var.tf_lbl_environment}"
+    ProvisionedBy       = "${var.tf_lbl_provisioned_by}"
+    SourceRepo          = "${var.tf_lbl_source_repo}"
+    ReleaseId           = "${var.tf_lbl_releaseid}"
+    BuildId             = "${var.tf_lbl_buildid}"    
   }
 }
 
 resource "azurerm_eventgrid_event_subscription" "eventgridsubscription" {
     depends_on = [azurerm_eventgrid_topic.eventgridtopic]
-    count = "${length(var.flasheventgridsubscription)}"
-    name  = "${lookup(var.flasheventgridsubscription[count.index], "name")}"
-    scope = "${azurerm_resource_group.rg.id}/providers/Microsoft.EventGrid/topics/${lookup(var.flasheventgridsubscription[count.index], "scope")}"
-    included_event_types  = list("${lookup(var.flasheventgridsubscription[count.index], "included_event_types")}")
+    count = "${length(var.eventgridsubscription)}"
+    name  = "${lookup(var.eventgridsubscription[count.index], "name")}"
+    scope = "${azurerm_resource_group.rg.id}/providers/Microsoft.EventGrid/topics/${lookup(var.eventgridsubscription[count.index], "scope")}"
+    included_event_types  = list("${lookup(var.eventgridsubscription[count.index], "included_event_types")}")
     storage_blob_dead_letter_destination {
-      storage_account_id = "${data.azurerm_resource_group.storageaccount.id}/providers/Microsoft.Storage/storageAccounts/${lookup(var.flasheventgridsubscription[count.index], "storage_account_id")}"
-      storage_blob_container_name = "${lookup(var.flasheventgridsubscription[count.index], "storage_blob_container_name")}"
+      storage_account_id = "${data.azurerm_resource_group.storageaccount.id}/providers/Microsoft.Storage/storageAccounts/${lookup(var.eventgridsubscription[count.index], "storage_account_id")}"
+      storage_blob_container_name = "${lookup(var.eventgridsubscription[count.index], "storage_blob_container_name")}"
     }
     retry_policy {
-      max_delivery_attempts = "${lookup(var.flasheventgridsubscription[count.index], "max_delivery_attempts")}"
-      event_time_to_live = "${lookup(var.flasheventgridsubscription[count.index], "event_time_to_live")}"
+      max_delivery_attempts = "${lookup(var.eventgridsubscription[count.index], "max_delivery_attempts")}"
+      event_time_to_live = "${lookup(var.eventgridsubscription[count.index], "event_time_to_live")}"
     }
     dynamic "webhook_endpoint" {
-      for_each = "${lookup(var.flasheventgridsubscription[count.index], "webhook_endpoint")}" == null ? [] : list(var.flasheventgridsubscription[count.index])
+      for_each = "${lookup(var.eventgridsubscription[count.index], "webhook_endpoint")}" == null ? [] : list(var.eventgridsubscription[count.index])
       content {
         url = "${webhook_endpoint.value.webhook_endpoint}${var.subscriptionkey}"
       }
