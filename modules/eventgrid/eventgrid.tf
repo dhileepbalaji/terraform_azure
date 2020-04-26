520cbc7b57268596d4589189ca78ac6ddc473529
dhileepbalaji <33687091+dhileepbalaji@users.noreply.github.com>
2020-04-26 11:08:39 +0530

    Update eventgrid.tf

--- modules/eventgrid/eventgrid.tf
+++ modules/eventgrid/eventgrid.tf
@@ -1,28 +1,6 @@
-variable "tf_lbl_cost_center" {}
-variable "tf_lbl_product" {}
-variable "tf_lbl_deployment_name" {}
-variable "tf_lbl_owner" {}
-variable "tf_lbl_environment" {}
-variable "tf_lbl_provisioned_by" {}
-variable "tf_lbl_source_repo" {}
-variable "tf_lbl_releaseid" {}
-variable "tf_lbl_buildid" {}
-variable "resource_group" {}
-variable "location" {}
-variable "eventgrid" {}
-variable "eventgridsubscription" {}
-variable "storageaccount" {}
-variable "subscriptionkey" {}
-
-
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group}"
   location = "${var.location}"
-}
-
-data "azurerm_resource_group" "storageaccount" {
-  name = "${var.storageaccount}"
 }
 
 resource "azurerm_eventgrid_topic" "eventgridtopic" {
